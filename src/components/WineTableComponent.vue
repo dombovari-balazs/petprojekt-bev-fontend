6099dbf69c9f2eb6fc6a0207f0d76ce0bda41946
Dombi <dbalazs1219@gmail.com>
2019-11-23 18:20:12 +0100

    Wine table root added

---
+++ src/components/WineTableComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name: WineTableComponent,
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/WineTable.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="winetable">
+      <h1>Hello! I'm a WineTable!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name : 'WineTable'
+}
+</script>
+
+<style>
+
+</style>
