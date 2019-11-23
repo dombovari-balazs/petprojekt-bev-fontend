516e88351835eca257e9b413c6c42c079fb4f482
Dombi <dbalazs1219@gmail.com>
2019-11-23 21:04:40 +0100

    Pre-made layout and pre-made table added
    Also had to fix routing, when vuetify install messed up App.vue

--- src/views/WineTable.vue
+++ src/views/WineTable.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="hello-message">
-      <h1>Hello! I'm a WineTable!</h1>
-  </div>
-  
+    <div>
+        <h1>
+            Hello I'm a WineTable!
+        </h1>
+    <WineTableComponent/>
 
+    </div> 
 </template>
 
 <script>
+import  WineTableComponent  from "../components/WineTableComponent.vue";
 export default {
-    name : 'WineTable'
+    name : 'WineTable',
+    components : {
+        WineTableComponent,
+    },
 }
 </script>
 
